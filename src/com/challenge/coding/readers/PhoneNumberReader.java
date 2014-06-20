2b657e1053a0102cd1819d207d414238d624846b
Jayan K <jayan143@gmail.com>
2014-06-20 11:25:11 +0530

    Reading default phone numbers
    Reading default phone numbers

--- src/com/challenge/coding/readers/PhoneNumberReader.java
+++ src/com/challenge/coding/readers/PhoneNumberReader.java
@@ -1,7 +1,11 @@
 package com.challenge.coding.readers;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
+import com.challenge.coding.config.CommonConfig;
+import com.challenge.coding.entities.PhoneNumbers;
+import com.challenge.coding.messages.Messages;
 import com.challenge.coding.readers.actions.Readable;
 
 public class PhoneNumberReader implements Readable{
@@ -14,6 +18,26 @@ public class PhoneNumberReader implements Readable{
 	public void read() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public PhoneNumbers read(BufferedReader reader){
+		
+		PhoneNumbers phoneNumbers = new PhoneNumbers();
+		String lineRead = "";
+		if(reader != null){
+			try {
+				while((lineRead = reader.readLine()) != null){
+					lineRead = lineRead.trim(); 
+					if(lineRead != ""){
+						phoneNumbers.addNumber(lineRead);
+					}
+				}
+			} catch (IOException e) {
+				CommonConfig.INSTANCE.getLogger().logError(Messages.INVALID_NUMBER_IN_PHONE_NUMBER);
+			}
+		}
+
+		return phoneNumbers;
 	}
 
 
--- src/com/challenge/coding/readers/actions/Readable.java
+++ src/com/challenge/coding/readers/actions/Readable.java
@@ -2,10 +2,14 @@ package com.challenge.coding.readers.actions;
 
 import java.io.BufferedReader;
 
+import com.challenge.coding.entities.BaseEntity;
+
 public interface Readable {
 
 	
 	public abstract void read();
+	
+	public abstract BaseEntity read(BufferedReader reader);
 	
 	
 }
